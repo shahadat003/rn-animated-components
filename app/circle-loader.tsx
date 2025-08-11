2aab32c26972ace830d184f5eeb40bce389f6929
ogulcanbagatir <ogulcan.bagatir@beynex.com>
2025-08-11 21:10:27 +0300

    circle-loader component example fix

--- app/circle-loader.tsx
+++ app/circle-loader.tsx
@@ -1,23 +1,17 @@
 import CircleLoader from "@/components/CircleLoader/CircleLoader"
 import colors from "@/theme/colors"
-import React, { useState } from "react"
-import { Pressable, StyleSheet, Text, View } from "react-native"
+import React, { useEffect, useState } from "react"
+import { StyleSheet, View } from "react-native"
 
 export default function CircleLoaderExample() {
-  const [play, setPlay] = useState(false)
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={()=> setPlay(p => !p)} style={{marginBottom: 32}}>
-        <Text style={styles.playText}>
-          {play ? "Reset" : "Play"}
-        </Text>
-      </Pressable>
       <CircleLoader
-        radius={80}
-        circleR={4}
-        colors={["#EF5A57", "#F5AF00", "#4BAA4E","#EF5A57", "#F5AF00", "#4BAA4E"]}
-        isPlaying={play}
+        radius={150}
+        circleR={8}
+        colors={[colors.p2, colors.orange1]}
+        isPlaying={true}
       />
     </View>
   )
